ea0ddf3dda261a18219234c384ee33555ab1b3c1
fastboy <giagiang200@gmail.com>
2022-01-23 14:57:02 +0700

    QR Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'QR';
+  public QrInfo='https://www.facebook.com/gia.giang.18/';
+  public width =200;
+
+  public change(event:any): void{
+    console.log('event=', event.target.value);
+    this.QrInfo = event.target.value
+  }
 }
